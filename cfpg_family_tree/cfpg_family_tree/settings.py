983f8c1fe4d1f1af6d2a38188ff7d44f8ddcbaac
Alex Rogers <alex.rogers823@gmail.com>
2020-01-25 00:43:02 -0500

    removed secret key from settings

--- cfpg_family_tree/cfpg_family_tree/settings.py
+++ cfpg_family_tree/cfpg_family_tree/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from ..cfpg_family_tree import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't167o31zd==d!65abws%!v+w-au2m5z4c7mz=i#oyzfc53ic_d'
+SECRET_KEY = env.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
